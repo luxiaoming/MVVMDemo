7048f238187e35485f338256daf3ef88f0e5b70d
luxiaoming <luxiaoming@wingtech.com>
2016-07-22 13:53:54 +0800

    提交测试demo

---
+++ app/src/main/java/com/xm/mvvmdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.xm.mvvmdemo;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xm.mvvmdemo.Model.User;
+import com.xm.mvvmdemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String str="代码GG之家微信号\n" +
+            "code_gg_home\n" +
+            "欢迎关注！";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+        User user = new User();
+        user.setName(str);
+        binding.setUser(user);
+    }
+
+}
---
+++ app/src/main/java/com/xm/mvvmdemo/Model/User.java
@@ -0,0 +1,22 @@
+package com.xm.mvvmdemo.Model;
+
+public class User {
+
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
