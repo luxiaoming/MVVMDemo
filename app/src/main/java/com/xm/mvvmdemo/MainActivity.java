5668dcbe70e2a07f51cfa85f21d35d8ac8302213
luxiaoming <luxiaoming@wingtech.com>
2016-07-24 19:35:59 +0800

    新增@BindingAdapter使用

--- app/src/main/java/com/xm/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/xm/mvvmdemo/MainActivity.java
@@ -12,16 +12,18 @@ public class MainActivity extends AppCompatActivity {
     private final String str="代码GG之家微信号\n" +
             "code_gg_home\n" +
             "欢迎关注！";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         ActivityMainBinding binding = DataBindingUtil.setContentView(
                 this, R.layout.activity_main);
-
-		
         User user = new User();
         binding.setUser(user);
         user.setName(str);
+        user.setAge(28);
+
     }
 
 }
--- app/src/main/java/com/xm/mvvmdemo/Model/User.java
+++ app/src/main/java/com/xm/mvvmdemo/Model/User.java
@@ -2,13 +2,18 @@ package com.xm.mvvmdemo.Model;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.widget.TextView;
 
 import com.xm.mvvmdemo.BR;
 
-public class User extends BaseObservable{
+public class User extends BaseObservable {
 
     @Bindable
     private String name;
+
+    @Bindable
+    private int age;
 
     public User() {
     }
@@ -17,10 +22,23 @@ public class User extends BaseObservable{
         this.name = name;
     }
 
+    @BindingAdapter("age")
+    public static void setAge(TextView text, int age) {
+        text.setText(age + "");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        notifyPropertyChanged(BR.age);
+    }
+
     public String getName() {
         return name;
     }
-
 
     public void setName(String name) {
         this.name = name;
